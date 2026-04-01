ebe34951e78765cd61b09552aa3a4401b7c20682
Dhaval Patil| BeyonData Group <dhaval.patil@beyondata.local>
2026-04-01 18:30:03 +0530

    add supabase support

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['pg'],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
